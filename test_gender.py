d6e057e816351539d1eee3036105c9928b1af36f
Splayser <90519305+Splayser@users.noreply.github.com>
2022-03-10 15:46:20 +0500

    Create test_gender.py

---
+++ test_gender.py
@@ -0,0 +1,96 @@
+import argparse
+import os
+import pandas as pd
+import numpy as np
+import os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping
+from sklearn.model_selection import train_test_split
+
+import librosa
+import numpy as np
+
+def extract_feature(file_name, **kwargs):
+    """
+    Extract feature from audio file `file_name`
+        Features supported:
+            - MFCC (mfcc)
+            - Chroma (chroma)
+            - MEL Spectrogram Frequency (mel)
+            - Contrast (contrast)
+            - Tonnetz (tonnetz)
+        e.g:
+        `features = extract_feature(path, mel=True, mfcc=True)`
+    """
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    X, sample_rate = librosa.core.load(file_name)
+    if chroma or contrast:
+        stft = np.abs(librosa.stft(X))
+    result = np.array([])
+    if mfcc:
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = np.hstack((result, mfccs))
+    if chroma:
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, chroma))
+    if mel:
+        mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, mel))
+    if contrast:
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+        result = np.hstack((result, contrast))
+    if tonnetz:
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+        result = np.hstack((result, tonnetz))
+    return result
+
+def create_model(vector_length=128):
+    model = Sequential()
+    model.add(Dense(256, input_shape=(vector_length,)))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.3))
+    # one output neuron with sigmoid activation function, 0 means female, 1 means male
+    model.add(Dense(1, activation="sigmoid"))
+    # using binary crossentropy as it's male/female classification (binary)
+    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
+    # print summary of the model
+    model.summary()
+    return model
+
+parser = argparse.ArgumentParser(description="""Gender recognition script, this will load the model you trained, 
+                                    and perform inference on a sample you provide (either using your voice or a file)""")
+parser.add_argument("-f", "--file", help="The path to the file, preferred to be in WAV format")
+args = parser.parse_args()
+file = args.file
+# construct the model
+model = create_model()
+# load the saved/trained weights
+model.load_weights("C:\\Users\\Splayser_lab\\OneDrive\\Рабочий стол\\стц\\nice\\model.h5")
+if not file or not os.path.isfile(file):
+    # if file not provided, or it doesn't exist, use your voice
+    print("Please talk")
+    # put the file name here
+    file = "test.wav"
+    # record the file (start talking)
+    record_to_file(file)
+# extract features and reshape it
+features = extract_feature(file, mel=True).reshape(1, -1)
+# predict the gender!
+male_prob = model.predict(features)[0][0]
+female_prob = 1 - male_prob
+gender = "male" if male_prob > female_prob else "female"
+# show the result!
+print("Result:", gender)
+print(f"Probabilities::: Male: {male_prob*100:.2f}%    Female: {female_prob*100:.2f}%")
